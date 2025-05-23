6aa46b47d7f3c3e9908f265d2b761b8bf9995cf7
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2025-05-23 18:41:05 -0300

    Create main3.py

---
+++ main3.py
@@ -0,0 +1,12 @@
+# arquivo: main3.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"mensagem": "Minha API "}
+
+@app.get("/soma")
+def somar(a: int, b: int):
+    return {"resultado": a + b}
